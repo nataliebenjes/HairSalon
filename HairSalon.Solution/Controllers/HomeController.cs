8664d63c5518259aca53e90c0b0e9ba7bccc9f0a
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-31 07:17:19 -0700

    Add dump file. Add image from SQL designer to README

---
+++ HairSalon.Solution/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairSalon.Solution.Controllers;
+
+public class HomeController : Controller
+{
+
+  [HttpGet("/")]
+  public ActionResult Index()
+  {
+    return View();
+  }
+
+}
---
+++ HairSalon.Solution/Models/Client.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public int StylistId { get; set; }
+    public string ClientName { get; set; }
+    public string ClientDetails { get; set; }
+    public Stylist Stylist { get; set; }
+  }
+}
